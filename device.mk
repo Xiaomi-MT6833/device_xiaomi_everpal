354243e463600bf5e0e6279d4e81b3bca598b44c
Jabiyeff <cebiyevanar@gmail.com>
2022-10-16 18:34:14 +0400

    everpal: parts: Import XiaomiParts for refresh rate control
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -471,3 +471,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+
+# XiaomiParts
+PRODUCT_PACKAGES += \
+    XiaomiParts
