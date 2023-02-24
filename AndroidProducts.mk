74a090f2eaa0f1dd4cdfe9c2ed5a6b6ff5fa7ac5
Taqi Raza <mttalpur@gmail.com>
2023-02-24 10:34:04 +0500

    everpal: add lineageos build support

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,7 +6,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_everpal.mk \
-    $(LOCAL_DIR)/arrow_everpal.mk
+    $(LOCAL_DIR)/arrow_everpal.mk \
+    $(LOCAL_DIR)/lineage_everpal.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_everpal-user \
@@ -14,4 +15,7 @@ COMMON_LUNCH_CHOICES := \
     aosp_everpal-eng \
     arrow_everpal-user \
     arrow_everpal-userdebug \
-    arrow_everpal-eng
+    arrow_everpal-eng \
+    lineage_everpal-user \
+    lineage_everpal-userdebug \
+    lineage_everpal-eng
---
+++ lineage_everpal.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit Vendor GApps
+$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
+
+# Inherit from everpal device
+$(call inherit-product, device/xiaomi/everpal/device.mk)
+
+# LineageOS Stuff
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_AOSP_RECOVERY := true
+
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_everpal
+PRODUCT_DEVICE := everpal
+PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := everpal
+
+# Build info
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
