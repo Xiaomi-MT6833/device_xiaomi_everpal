b281c6b075850e39ab8df01dfaa67fae35c343ad
Jax Lee <sd369888@outlook.com>
2023-03-11 14:42:03 +0800

    everpal: evox: target enable blur

--- evolution_everpal.mk
+++ evolution_everpal.mk
@@ -22,6 +22,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_AOSP_RECOVERY := true
+TARGET_ENABLE_BLUR := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := evolution_everpal
