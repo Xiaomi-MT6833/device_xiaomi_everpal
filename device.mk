69dbaa5bd8e7b4991c683f1cc334f0db62cd543f
Sushrut1101 <guptasushrut@gmail.com>
2023-04-07 19:49:19 +0530

    everpal: Drop HIDL Bluetooth audio interfaces
    * We're using AIDL Bluetooth Audio
    
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -98,7 +98,6 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio@2.1.vendor \
     android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth@1.1.vendor
 
